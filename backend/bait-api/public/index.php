95267c9ae1c8e28d111ee25262f6438706c83f5c
ivanleirbag <ivanpeceto20@gmail.com>
2025-08-19 18:24:54 -0300

    Merge

--- backend/bait-api/public/index.php
+++ backend/bait-api/public/index.php
@@ -17,10 +17,4 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-<<<<<<< HEAD
-=======
-// Force Laravel to search .env on the root folder
-$app->useEnvironmentPath(dirname(__DIR__, 2));
-
->>>>>>> 4d5abedd59629e8e3868b54bce5fddae5285d0eb
 $app->handleRequest(Request::capture());
