71dd18be5ad441b5824b8c3d1677e5eaf3bcbc56
ivanleirbag <ivanpeceto20@gmail.com>
2025-08-28 16:37:57 -0300

    Added prefix /api to all routes

--- backend/bait-api/routes/api.php
+++ backend/bait-api/routes/api.php
@@ -7,52 +7,54 @@ use App\Modules\UserData\Http\Controllers\UserRoleController;
 use App\Modules\UserData\Http\Controllers\AvatarController;
 use App\Modules\Multimedia\Http\Controllers\PostController;
 
-/*Healthcheck routes*/
+Route::prefix('api')->group(function () {
+    /*Healthcheck routes*/
 
-Route::get('/', function () {
-    return response()->json(['status' => 'ok']);
-});
+    Route::get('/', function () {
+        return response()->json(['status' => 'ok']);
+    });
 
-Route::get('/api/ping', function () {
-    return response()->json(['status' => 'ok']);
-});
+    Route::get('/ping', function () {
+        return response()->json(['status' => 'ok']);
+    });
 
-/*------------------*/
+    /*------------------*/
 
-Route::prefix('auth')->group(function () {
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('login',    [AuthController::class, 'login']);
-    Route::post('refresh',  [AuthController::class, 'refresh'])->middleware('auth:api');
-    Route::post('logout',   [AuthController::class, 'logout'])->middleware('auth:api');
-    Route::get('me',        [AuthController::class, 'me'])->middleware('auth:api');
-});
+    Route::prefix('auth')->group(function () {
+        Route::post('register', [AuthController::class, 'register']);
+        Route::post('login',    [AuthController::class, 'login']);
+        Route::post('refresh',  [AuthController::class, 'refresh'])->middleware('auth:api');
+        Route::post('logout',   [AuthController::class, 'logout'])->middleware('auth:api');
+        Route::get('me',        [AuthController::class, 'me'])->middleware('auth:api');
+    });
 
-Route::middleware('auth:api')->prefix('profile')->group(function () {
-    Route::get('/',            [ProfileController::class, 'show']);
-    Route::put('/',            [ProfileController::class, 'update']);
-    Route::put('/password',    [ProfileController::class, 'changePassword']);
-    Route::post('/avatar',     [ProfileController::class, 'updateAvatar']);
-    Route::post('/banner',     [ProfileController::class, 'updateBanner']);
-});
+    Route::middleware('auth:api')->prefix('profile')->group(function () {
+        Route::get('/',            [ProfileController::class, 'show']);
+        Route::put('/',            [ProfileController::class, 'update']);
+        Route::put('/password',    [ProfileController::class, 'changePassword']);
+        Route::post('/avatar',     [ProfileController::class, 'updateAvatar']);
+        Route::post('/banner',     [ProfileController::class, 'updateBanner']);
+    });
 
-Route::prefix('roles')->group(function () {
-    Route::get('/', [UserRoleController::class, 'index']);
-    Route::get('/', [UserRoleController::class, 'create']);
-    Route::get('/{role}', [UserRoleController::class, 'update']);
-    Route::get('/{role}', [UserRoleController::class, 'destroy']);
+    Route::prefix('roles')->group(function () {
+        Route::get('/', [UserRoleController::class, 'index']);
+        Route::get('/', [UserRoleController::class, 'create']);
+        Route::get('/{role}', [UserRoleController::class, 'update']);
+        Route::get('/{role}', [UserRoleController::class, 'destroy']);
 
-});
+    });
 
-Route::prefix('avatars')->middleware('auth:api')->group(function () {
-    Route::post('/upload', [AvatarController::class, 'upload'])->name('avatars.upload');
+    Route::prefix('avatars')->middleware('auth:api')->group(function () {
+        Route::post('/upload', [AvatarController::class, 'upload'])->name('avatars.upload');
 
-    Route::get('/{id}', [AvatarController::class, 'show'])->name('avatars.show');
-});
+        Route::get('/{id}', [AvatarController::class, 'show'])->name('avatars.show');
+    });
 
-Route::middleware('auth:api')->prefix('posts')->group(function () {
-    Route::get('/', [PostController::class, 'index']);
-    Route::post('/', [PostController::class, 'store']);
-    Route::get('/{post}', [PostController::class, 'show']);
-    Route::put('/{post}', [PostController::class, 'update']);
-    Route::delete('/{post}', [PostController::class, 'destroy']);
+    Route::middleware('auth:api')->prefix('posts')->group(function () {
+        Route::get('/', [PostController::class, 'index']);
+        Route::post('/', [PostController::class, 'store']);
+        Route::get('/{post}', [PostController::class, 'show']);
+        Route::put('/{post}', [PostController::class, 'update']);
+        Route::delete('/{post}', [PostController::class, 'destroy']);
+    });
 });
