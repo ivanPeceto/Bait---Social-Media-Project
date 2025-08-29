2e35363cebbf5e5e6476c9e8c24545ed7d189796
ivanleirbag <ivanpeceto20@gmail.com>
2025-08-29 16:29:55 -0300

    Created user factory for testing

---
+++ backend/bait-api/database/factories/Modules/UserData/Domain/Models/UserFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories\Modules\UserData\Domain\Models;
+
+use App\Modules\UserData\Domain\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
+class UserFactory extends Factory
+{
+    protected $model = User::class;
+
+    public function definition()
+    {
+        return [
+            'username' => $this->faker->unique()->userName(),
+            'name'     => $this->faker->name(),
+            'email'    => $this->faker->unique()->safeEmail(),
+            'password' => Hash::make('password'), 
+            'role_id'  => 1, 
+            'state_id' => 1,
+        ];
+    }
+}
--- backend/bait-api/tests/TestCase.php
+++ backend/bait-api/tests/TestCase.php
@@ -7,13 +7,4 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->app['auth']->extend('jwt', function ($app, $name, array $config) {
-            return $app->make(\PHPOpenSourceSaver\JWTAuth\JWTGuard::class);
-        });
-    }
 }
