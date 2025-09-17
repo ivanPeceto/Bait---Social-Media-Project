d2a52f0c14846e988cbb17d5108a93945a9a4ca5
ivanleirbag <ivanpeceto20@gmail.com>
2025-09-17 18:04:19 -0300

    feat(back): Adds new messsagges handling on the webscoket

--- backend/bait-api/routes/channels.php
+++ backend/bait-api/routes/channels.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Modules\UserInteractions\Domain\Models\Chat;
 use Illuminate\Support\Facades\Broadcast;
+
 
 if (! config('broadcasting.connections.reverb.key')) {
     return;
@@ -9,4 +11,9 @@ if (! config('broadcasting.connections.reverb.key')) {
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('chat.{chat_id}', function ($user, $chat_id){
+    $chat = Chat::find($chat_id);
+    return $chat && $chat->users->contains($user);
 });
