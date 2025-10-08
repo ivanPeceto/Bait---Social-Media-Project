7445d684267c82a7592804718e0382eb8d84e214
ivan <ivanpeceto20@gmail.com>
2025-10-08 19:06:59 -0300

    merge fix

--- backend/bait-api/database/seeders/AvatarSeeder.php
+++ backend/bait-api/database/seeders/AvatarSeeder.php
@@ -12,17 +12,8 @@ class AvatarSeeder extends Seeder
      */
     public function run(): void
     {
-<<<<<<< HEAD
         DB::table('avatars')->insert([
             'url_avatars' => 'avatars/default.jpg',
-=======
-        // Corregido a 'avatars' (plural) para que coincida con la tabla
-        DB::table('avatars')->insert([
-            // Corregido a 'url_avatars' (plural) para que coincida con la columna
-            'url_avatars' => 'avatars/default.jpg',
-            'created_at' => now(), 
-            'updated_at' => now()
->>>>>>> feature/backend/documentation
         ]);
     }
 }
