443d67602c53d50fef45af387adbae26c6b9bfb1
jmrodriguezspinker <eionoman21@gmail.com>
2025-10-12 21:06:22 -0300

    fix: cambiar la longitud mínima de la contrseña a 8

--- frontend/src/app/features/auth/login/login.ts
+++ frontend/src/app/features/auth/login/login.ts
@@ -43,7 +43,7 @@ export default class LoginComponent {
    */
   public loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    password: new FormControl('', [Validators.required, Validators.minLength(8)])
   });
 
   /**
