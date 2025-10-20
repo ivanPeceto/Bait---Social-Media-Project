891ed32210102a56b563b6b10420224537fe1d87
juancruzCT <juancruzctclases12@gmail.com>
2025-10-20 18:44:06 -0300

    fixing conflicts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,9 +1,5 @@
 export const environment = {
   production: false,
-<<<<<<< HEAD
   apiUrl: 'http://localhost:8001/api',
   baseUrl: 'http://localhost:8001'
-=======
-  apiUrl: 'http://localhost:8001/api' 
->>>>>>> origin/feature/frontend/search
 };
